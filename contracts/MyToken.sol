ddbd317707e8951455a140d419893848d0824a71
jiu0u <pllpopo@gmai.com>
2025-10-12 05:56:02 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+    
     string public name;
     string public symbol;
     uint8 public decimals;
@@ -11,25 +14,50 @@ contract MyToken {
 
     //누가 얼마나 가지고 있는지
     mapping(address => uint256) public balanceOf;
-    // (key => value)
+    mapping(address => mapping(address => uint256)) allowance;
+
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        _mint(_amount *10**uint256(decimals), msg.sender); // amount만큼 MT 발행
+        _mint(_amount *10**uint256(decimals), msg.sender); // _amount만큼 MT 발행
     }
+
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount; 
+                
+        emit Transfer(address(0), owner, amount);
     }
+
 
     function transfer(uint256 amount, address to) external {
         require(balanceOf[msg.sender] >= amount, "insufficient balance");
 
         balanceOf[msg.sender] -= amount; 
         balanceOf[to] += amount; 
+
+        emit Transfer(msg.sender, to, amount);
     }
- 
+
+
+
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(spender, amount);
+    }
+
+    function TransferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        
+        emit Transfer(from, to, amount);
+    }
 }
