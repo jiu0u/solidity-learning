f6cd36fcb3bd7fc3c4d91a89641cbf880d599014
jiu0u <pllpopo@gmai.com>
2025-10-30 15:34:29 +0900

    add reward logic

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -5,15 +5,20 @@ interface IMyToken {
     // MyToken.sol에서 사용할 함수의 헤더 가져오기
     function transfer(uint256 amount, address to) external;
     function transferFrom(address from, address to, uint256 amount) external;
-    
+    function mint(uint256 amount, address owner) external;
 }
 
 
 
 contract TinyBank {
     event Staked(address, uint256);
+    event Withdraw(uint256 amount, address to);
 
     IMyToken public stakingToken; 
+
+    mapping(address => uint256) public lastClaimedBlock; //reawrd 기준점.
+    uint256 rewardPerBlock = 1*10 ** 18;
+    
     mapping(address => uint256) public staked; 
     uint256 public totalStaked; 
 
@@ -26,9 +31,29 @@ contract TinyBank {
 
     function stake(uint256 _amount) external {
         require(_amount >= 0, "cannot stake 0 amount");
+        distributeReward(msg.sender);
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
     }
+
+
+    function withdraw(uint256 _amount) external {
+        require(staked[msg.sender] >= _amount, "insufficient staked token" );
+        distributeReward(msg.sender);
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        emit Withdraw(_amount, msg.sender);
+    }
+
+
+    function distributeReward(address to) internal {
+        uint256 blocks = block.number - lastClaimedBlock[to];
+        uint256 reward = (blocks * rewardPerBlock * staked[to]) / totalStaked ;
+        stakingToken.mint(reward, to);
+        lastClaimedBlock[to] = block.number;
+    } //공평한 분배는 아님. 하지만 다루지 않음.
+
 }
