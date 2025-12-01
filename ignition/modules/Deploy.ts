5f4a4c525957a67e5d2089569024c138508345ce
jiu0u <pllpopo@gmai.com>
2025-12-01 22:28:19 +0900

    fix(Deploy): ensure TinyBank is set as manager for MyToken

--- ignition/modules/Deploy.ts
+++ ignition/modules/Deploy.ts
@@ -5,6 +5,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("MyTokenDeploy", (m) => {
   const myTokenC = m.contract("MyToken", ["MyToken", "MT", 18, 100]);
   const tinyBankC = m.contract("TinyBank", [myTokenC]);
+  m.call(myTokenC, "setManager", [tinyBankC]);
   return { myTokenC, tinyBankC };
   // 오브젝트로 감싸서 리턴해줘야 한다?
 });
